c35ef067dfe06c18ece9f821a1b8473f674d6d52
sabertazimi <sabertazimi@gmail.com>
2018-07-19 17:28:15 +0800

    update(app-render): force re-render every time `dispatch` got invoked

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
 
 import store from './store.js';
-import createAction from './action.js';
-import {
-    CREATE_NOTE,
-    UPDATE_NOTE
-} from './constants.js';
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
-        store.dispatch(createAction(CREATE_NOTE));
-        store.dispatch(createAction(UPDATE_NOTE, 1, 'Hello React'));
-        store.dispatch(createAction(CREATE_NOTE));
-        store.dispatch(createAction(UPDATE_NOTE, 2, 'Hello Medux'));
+    }
+
+    componentDidUpdate() {
+        console.log('> componentDidUpdate ...');
     }
 
     render() {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import store from './store.js';
+import createAction from './action.js';
+import {
+    CREATE_NOTE,
+    UPDATE_NOTE
+} from './constants.js';
+
 import App from './App.js';
 
 import './index.css';
 
-ReactDOM.render((
-    <App />
-), document.getElementById('root'));
+store.subscribe(() => {
+    ReactDOM.render((
+        <App />
+    ), document.getElementById('root'));
+});
+
+store.dispatch(createAction(CREATE_NOTE));
+store.dispatch(createAction(UPDATE_NOTE, 1, 'Hello React'));
+store.dispatch(createAction(CREATE_NOTE));
+store.dispatch(createAction(UPDATE_NOTE, 2, 'Hello Medux'));
