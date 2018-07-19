251cf25cca9f01b07596ced28ddf59aac7db5afa
sabertazimi <sabertazimi@gmail.com>
2018-07-19 21:22:52 +0800

    add(medux-middleware): understand internal of middleware (redux-thunk)

--- src/medux/store.js
+++ src/medux/store.js
@@ -5,17 +5,37 @@ import {
     INIT_MEDUX
 } from './constants.js';
 
-const createStore = (reducer) => {
+const applyMiddleware = (...middlewares) => store => {
+    // should return (next) => (action) => { ... } function
+    if (middlewares.length === 0) {
+        return dispatch => dispatch;
+    }
+
+    if (middlewares.length === 1) {
+        return middlewares[0];
+    }
+
+    // [ (next) => (action) => {...}, ... ] array
+    // next: (action) => { ... } function
+    const boundMiddlewares = middlewares.map(middleware => middleware(store));
+
+    return boundMiddlewares.reduce((a, b) => (next => a(b(next))));
+};
+
+const createStore = (reducer, middleware) => {
     // clousre for storing global state
     let state = undefined;
     const subscribers = [];
+    const coreDispatch = (action) => {
+        validateAction(action);
+        state = reducer(state, action);
+        subscribers.forEach(handler => handler());
+    };
+    const getState = () => state;
+
     const store = {
-        dispatch: (action) => {
-            validateAction(action);
-            state = reducer(state, action);
-            subscribers.forEach(handler => handler());
-        },
-        getState: () => state,
+        dispatch: coreDispatch,
+        getState,
         subscribe: (handler) => {
             subscribers.push(handler);
 
@@ -30,7 +50,20 @@ const createStore = (reducer) => {
         }
     };
 
-    store.dispatch({
+    if (middleware) {
+        // store default dispatch
+        const dispatch = action => store.dispatch(action);
+
+        // const middleware = ({ dispatch, getState }) => (next) => (action) => { ... };
+        // middleware is a higher-order function (return a function (action) => { ... });
+        // dispatch, getState and coreDispatch are injected into middleware as arguments
+        store.dispatch = middleware({
+            dispatch,
+            getState
+        })(coreDispatch);
+    }
+
+    coreDispatch({
         type: INIT_MEDUX
     });
     return store;
